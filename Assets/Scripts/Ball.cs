65d8d94c19d2bb74a8bc8a755d51a09a36a8ce6f
Teoman TURAN <57498915+teomanturan@users.noreply.github.com>
2023-09-16 04:04:26 +0300

    Ball Speed Up

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,8 +12,9 @@ public class Ball : MonoBehaviour
     float x;
     float y;
     int score = 0;
-    [SerializeField] Text scoreText; 
-
+    [SerializeField] Text scoreText;
+    Vector2 direction;
+    public float coefficient = 0.1f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -24,25 +26,36 @@ public class Ball : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        Debug.Log(collision.collider.tag);
         if (collision.collider.CompareTag("Player"))
         {
             score++;
             scoreText.text = score.ToString();
-            x = -x;
-            movementSpeed = movementSpeed + 5;
-            rb.AddForce(new Vector2 (x, y));
+            SpeedUp();
+        }
+        if (collision.collider.CompareTag("DeathZone"))
+        {
+            rb.velocity = Vector2.zero;
         }
     }
-
     #region Top Baslangic Hareketi
     void StartingBallMovement()
     {
         x = UnityEngine.Random.value < 0.5f ? -1.0f : 1.0f;
         y = UnityEngine.Random.value < UnityEngine.Random.value ? UnityEngine.Random.Range(-1.0f, -0.5f) : UnityEngine.Random.Range(1.0f, 0.5f);
+        direction = new Vector2(x * movementSpeed, y * movementSpeed);
 
-        rb.AddForce(new Vector2 (x, y) * movementSpeed);
+        rb.AddForce(direction);
+    }
+    #endregion
+
+    #region Hýzlandýrma
+    void SpeedUp()
+    {
+        float tempcoeffx = coefficient;
+        float tempcoeffy = coefficient;
+        tempcoeffx = rb.velocity.x < 0 ? -tempcoeffx : tempcoeffx;
+        tempcoeffy = rb.velocity.y < 0 ? -tempcoeffy : tempcoeffy;
+        rb.velocity = new Vector2(rb.velocity.x + tempcoeffx, rb.velocity.y + tempcoeffy);
     }
     #endregion
 }
